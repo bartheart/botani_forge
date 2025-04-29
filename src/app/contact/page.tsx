7cde1fdaf07a6c263cfa5fd946f005c150980b88
bartheart <bemnetmerkebu17@gmail.com>
2025-04-28 21:35:43 -0500

    intital commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+      <main className="flex items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold">Contact Us</h1>
+      </main>
+    );
+  }
+  
