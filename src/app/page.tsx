15a4ccef165474ed1a38ac9002016e9a013cf159
bartheart <bemnetmerkebu17@gmail.com>
2025-05-01 23:39:36 -0500

    replaced the single qoutes with apostrophes in the main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <div>
+      <nav>
         <Link href="/">Home</Link>
         <Link href="/products">Products</Link>
         <Link href="/about">About Us</Link>
-      </div>
-      <div>
+      </nav>
+      <div className="flex">
         <div>
-          <h2>At [Your Business Name], gardens aren't just plants — they're memories.</h2>
-          <p>Our handcrafted beds are made to last and designed to make growing easy, whether you're a seasoned gardener or just starting out.</p>
-          <p>Let’s grow something amazing together.</p>
+          <h2>At [Your Business Name], gardens aren&apos;t just plants — they&apos;re memories.</h2>
+          <p>Our handcrafted beds are made to last and designed to make growing easy, whether you&apos;re a seasoned gardener or just starting out.</p>
+          <p>Let&apos;s grow something amazing together.</p>
           <Link href="/products">[Shop Now]</Link>
         </div>
         <div>
-          <img src="/garden_bed.jpeg"/>
+          <Image 
+            src="/garden_bed.jpeg"
+            alt="Garden Bed"
+            width={200}
+            height={200}
+            />
         </div>
       </div>
     </main>
